9c48b729177239aaa560b08c5bad480f4b171b07
sirMackk <c.tairix@gmail.com>
2013-01-17 11:15:29 -0500

    Static files and s3

--- blog/settings.py
+++ blog/settings.py
@@ -168,10 +168,12 @@ DATABASES['default'] = dj_database_url.config()
 if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ['AWS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_KEY']
-    
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET']
+    AWS_PRELOAD_METADATA = True    
 
     AWS_STORAGE = os.environ['AWS_STORAGE_BUCKET']
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE
     STATIC_URL = S3_URL
+    ADMIN_MEDIA_PREFIX = '%s/admin' % S3_URL
