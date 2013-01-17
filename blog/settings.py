1cad88826724db1fd946fdfefde78806a9175980
sirMackk <c.tairix@gmail.com>
2013-01-17 11:06:48 -0500

    More s3 troubleshooting

--- blog/settings.py
+++ blog/settings.py
@@ -166,7 +166,12 @@ import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
 if not DEBUG:
+    AWS_ACCESS_KEY_ID = os.environ['AWS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_KEY']
+    
+
     AWS_STORAGE = os.environ['AWS_STORAGE_BUCKET']
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE
     STATIC_URL = S3_URL
